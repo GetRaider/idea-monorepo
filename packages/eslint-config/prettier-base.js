4a2965771493b2153bac33a58eef561cdb08a320
Andrew Polovets <52605048+GetRaider@users.noreply.github.com>
2025-04-23 22:47:32 +0200

    feat: run api app via nodemon and ts to avoid using js files with dist (#1)
    * feat: run api app via nodemon
    
    * feat: make api package independant without dist
    
    * fix: build issue with undefined initial prop values
    
    * feat: change prettier base config

--- packages/eslint-config/prettier-base.js
+++ packages/eslint-config/prettier-base.js
@@ -1,4 +1,10 @@
 /** @type {import("prettier").Config} */
 module.exports = {
+  semi: true,
+  tabWidth: 2,
+  useTabs: false,
   singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  quoteProps: 'as-needed',
 };

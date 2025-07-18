dd9351991804b758d91876d9c00959a1d44c33ed
Main <andrewpolovec@gmail.com>
2025-07-18 22:09:19 +0200

    feat: set some new files

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from '@modules/app.module';
+import { links } from '@denzel/api/src';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  console.log({ asd: links.dto.CreateLinkDto });
 }
 bootstrap();

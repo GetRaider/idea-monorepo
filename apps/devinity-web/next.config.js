5c6bab7c8ee623d79223e831b9c5e2e7a6d35a1f
Main <andrewpolovec@gmail.com>
2025-09-22 23:36:36 +0200

    fix: remove unused api rewrite in next config

--- apps/devinity-web/next.config.js
+++ apps/devinity-web/next.config.js
@@ -4,13 +4,4 @@ module.exports = {
   compiler: {
     styledComponents: true,
   },
-  async rewrites() {
-    const apiBase = process.env.NEXT_PUBLIC_API_BASE_URL;
-    return [
-      {
-        source: "/api/:path*",
-        destination: `${apiBase}/api/:path*`,
-      },
-    ];
-  },
 };

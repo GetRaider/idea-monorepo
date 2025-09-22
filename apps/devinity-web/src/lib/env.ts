20e60a05217a48946448c409c2bf662e6df83330
Main <andrewpolovec@gmail.com>
2025-09-22 19:28:22 +0200

    fix: load .env for devinity-web the same way as devinity-api

--- apps/devinity-web/src/lib/env.ts
+++ apps/devinity-web/src/lib/env.ts
@@ -1,19 +1,44 @@
 import { z } from "zod";
+import { config } from "dotenv";
+import { resolve } from "path";
 
-const envConfig = {
-  // NEXT_PUBLIC_ vars should be explicitly set
-  NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
-  ...process.env,
-};
+loadEnvFile();
 
 const envSchema = z.object({
   NEXT_PUBLIC_API_BASE_URL: z.string().url(),
 });
 
-const parsedEnv = envSchema.parse(envConfig);
+const processEnv = {
+  // NEXT_PUBLIC_ vars should be explicitly set like this to avoid Zod errors
+  NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  ...process.env,
+};
+
+const parsedEnv = envSchema.parse(processEnv);
 
 export const env = {
   api: {
     baseUrl: parsedEnv.NEXT_PUBLIC_API_BASE_URL,
   },
 };
+
+function loadEnvFile(): void {
+  let path: string | null = null;
+  const nodeEnv = process.env.NODE_ENV || "development";
+  const envFileName =
+    nodeEnv === "production" ? ".env.production" : ".env.local";
+
+  if (nodeEnv === "production") {
+    // loads .env.production from root directory because it sets there on deployments
+    const isInAppsDir = process.cwd().includes("/apps/devinity-web");
+    const rootDir = isInAppsDir
+      ? resolve(process.cwd(), "../../")
+      : process.cwd();
+    path = resolve(rootDir, envFileName);
+  } else {
+    // loads .env.local from current directory
+    path = resolve(process.cwd(), envFileName);
+  }
+
+  config({ path });
+}

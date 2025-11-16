5ce0b160fb6d6ee227277ffafcfb533269d46526
Main <andrewpolovec@gmail.com>
2025-11-16 16:45:23 +0100

    feat: update base button and add new run commands

--- packages/ui/src/components/Button/Button.component.tsx
+++ packages/ui/src/components/Button/Button.component.tsx
@@ -1,12 +1,37 @@
-'use client';
-import { ReactNode, ButtonHTMLAttributes } from 'react';
-import { Button as ButtonRadix } from '@radix-ui/themes';
+"use client";
+import { ReactNode, ButtonHTMLAttributes } from "react";
+import { Button as ButtonRadix } from "@radix-ui/themes";
 
-type ButtonVariant = 'solid' | 'soft' | 'outline' | 'ghost';
-type ButtonSize = '1' | '2' | '3' | '4';
-type ButtonColor = 'gray' | 'gold' | 'bronze' | 'brown' | 'orange' | 'amber' | 'yellow' | 'lime' | 'grass' | 'green' | 'mint' | 'teal' | 'cyan' | 'sky' | 'blue' | 'indigo' | 'violet' | 'purple' | 'plum' | 'pink' | 'crimson' | 'red' | 'tomato' | 'ruby';
+type ButtonVariant = "solid" | "soft" | "outline" | "ghost";
+type ButtonSize = "1" | "2" | "3" | "4";
+type ButtonColor =
+  | "gray"
+  | "gold"
+  | "bronze"
+  | "brown"
+  | "orange"
+  | "amber"
+  | "yellow"
+  | "lime"
+  | "grass"
+  | "green"
+  | "mint"
+  | "teal"
+  | "cyan"
+  | "sky"
+  | "blue"
+  | "indigo"
+  | "violet"
+  | "purple"
+  | "plum"
+  | "pink"
+  | "crimson"
+  | "red"
+  | "tomato"
+  | "ruby";
 
-interface ButtonProps extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'color'> {
+interface ButtonProps
+  extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, "color"> {
   children: ReactNode;
   variant?: ButtonVariant;
   size?: ButtonSize;
@@ -14,24 +39,24 @@ interface ButtonProps extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'col
   className?: string;
   onClick?: () => unknown;
   disabled?: boolean;
-  type?: 'button' | 'submit' | 'reset';
+  type?: "button" | "submit" | "reset";
 }
 
-export const Button = ({ 
-  children, 
-  className, 
+export const Button = ({
+  children,
+  className,
   onClick,
-  variant = 'solid',
-  size = '3',
+  variant = "solid",
+  size = "3",
   color,
   disabled,
-  type = 'button',
+  type = "button",
   ...props
 }: ButtonProps) => {
   return (
-    <ButtonRadix 
-      className={className} 
-      size={size} 
+    <ButtonRadix
+      className={className}
+      size={size}
       variant={variant}
       color={color}
       onClick={onClick}
